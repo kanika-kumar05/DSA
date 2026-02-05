8a7d08c809e5db142685b8a0a261e2f57fcf9118
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-02-05 22:40:39 +0530

    Time: 1 ms (99.51%) | Memory: 47.6 MB (6.25%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int maxReach = 0;
+        int end = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+            
+            if (i == end) {
+                jumps++;
+                end = maxReach;
+            }
+        }
+        
+        return jumps;
+    }
+}
