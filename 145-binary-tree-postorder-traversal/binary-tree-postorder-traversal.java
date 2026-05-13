774a1a87676238b87e8cbf7725cf2110bdb893a9
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-05-13 13:35:04 +0530

    Time: 1 ms (12.50%) | Memory: 43 MB (69.29%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -14,17 +14,51 @@
  * }
  */
 class Solution {
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> arr=new ArrayList<>();
+    //     if(root==null) return arr;
+    //     helper(root,arr);
+    //     return arr;
+    // }
+    // public static void helper(TreeNode root,List<Integer> arr){
+    //     if(root==null) return;
+    //     helper(root.left,arr);
+    //     helper(root.right,arr);
+    //     arr.add(root.val);
+
+    // }
+
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> arr=new ArrayList<>();
         if(root==null) return arr;
-        helper(root,arr);
-        return arr;
-    }
-    public static void helper(TreeNode root,List<Integer> arr){
-        if(root==null) return;
-        helper(root.left,arr);
-        helper(root.right,arr);
-        arr.add(root.val);
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode curr=root;
+        TreeNode lastVisited=null;
 
-    }
+        while(curr!=null || !st.isEmpty()){
+            if(curr!=null){
+                st.push(curr);
+                curr=curr.left;
+            }
+            else{
+                TreeNode temp=st.peek().right;
+                if(temp==null){
+                    temp=st.peek();
+                    st.pop();
+                    arr.add(temp.val);
+                    while(!st.isEmpty() && temp==st.peek().right){
+                        temp=st.peek();
+                        st.pop();
+                        arr.add(temp.val);
+                    }
+
+                }
+                else{
+                    curr=temp;
+                }
+            }
+        }
+
+        return arr;
+        }
 }
