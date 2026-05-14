181644c99dcd302f5b09f3fafa6c52428ba33f0a
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-05-14 13:07:35 +0530

    Time: 1 ms (12.59%) | Memory: 43.2 MB (49.95%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -28,33 +28,56 @@ class Solution {
 
     // }
 
+    // public List<Integer> postorderTraversal(TreeNode root) {
+    //     List<Integer> arr=new ArrayList<>();
+    //     if(root==null)return arr;
+    //     Stack<TreeNode> st=new Stack<>();
+    //     TreeNode curr=root;
+        
+    //     while(curr!=null || !st.isEmpty()){
+    //         if(curr!=null){
+    //             st.push(curr);
+    //             curr=curr.left;
+    //         }
+    //         else{
+    //             TreeNode temp=st.peek().right;
+    //             if(temp==null){
+    //                 temp=st.peek();
+    //                 st.pop();
+    //                 arr.add(temp.val);
+    //                 while(!st.isEmpty() && temp==st.peek().right){
+    //                     temp=st.peek();
+    //                     st.pop();
+    //                     arr.add(temp.val);
+    //                 }
+    //             }
+    //             else{
+    //                 curr=temp;
+    //             }
+    //         }
+    //     }
+    //     return arr;
+
+
+
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> arr=new ArrayList<>();
-        if(root==null)return arr;
-        Stack<TreeNode> st=new Stack<>();
-        TreeNode curr=root;
-        
-        while(curr!=null || !st.isEmpty()){
-            if(curr!=null){
-                st.push(curr);
-                curr=curr.left;
-            }
-            else{
-                TreeNode temp=st.peek().right;
-                if(temp==null){
-                    temp=st.peek();
-                    st.pop();
-                    arr.add(temp.val);
-                    while(!st.isEmpty() && temp==st.peek().right){
-                        temp=st.peek();
-                        st.pop();
-                        arr.add(temp.val);
-                    }
-                }
-                else{
-                    curr=temp;
-                }
-            }
+        Stack<TreeNode> st1=new Stack<>();
+        Stack<TreeNode> st2=new Stack<>();
+       
+        if(root==null) return arr;
+        st1.push(root);
+       
+
+        while(!st1.isEmpty()){
+            TreeNode node=st1.pop();
+            st2.push(node);
+            if(node.left!=null) st1.push(node.left);
+            if(node.right!=null)st1.push(node.right);
+            
+        }
+        while(!st2.isEmpty()){
+            arr.add(st2.pop().val);
         }
         return arr;
     }
