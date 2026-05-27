3b80a81643ab7c66ddc5b29c1153ada826cad573
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-05-27 20:51:12 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (67.97%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/recover-binary-search-tree.java
@@ -1,52 +1,51 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    private TreeNode first = null;
-    private TreeNode second = null;
-    private TreeNode prev = null;
-
-    public void recoverTree(TreeNode root) {
-        inorderTraversal(root);
-        
-        if (first != null && second != null) {
-            int temp = first.val;
-            first.val = second.val;
-            second.val = temp;
-        }
-    }
-
-    private void inorderTraversal(TreeNode root) {
-        if (root == null) {
-            return;
-        }
-
-        inorderTraversal(root.left);
-
-        if (prev != null && root.val < prev.val) {
-            
-            if (first == null) {
-                first = prev; 
-                second = root;
-            } else {
-                second = root;
+    private TreeNode first;
+    private TreeNode middle;
+    private TreeNode last;
+    private TreeNode prev;
+    public void inorder(TreeNode root){
+        if(root==null)return;
+        inorder(root.left);
+        if(prev!=null && root.val<prev.val){
+            if(first==null){
+                first=prev;
+                middle=root;
+            }
+            else{
+                last=root;
             }
         }
-        
-        prev = root;
-
-        inorderTraversal(root.right);
+        prev=root;
+        inorder(root.right);
+    }
+    public void recoverTree(TreeNode root) {
+        first=middle=last=null;
+        prev=new TreeNode(Integer.MIN_VALUE);
+        inorder(root);
+        if(first!=null && last!=null){
+            int t=first.val;
+            first.val=last.val;
+            last.val=t;
+        }
+        else if(first!=null && middle!=null){
+            int t=first.val;
+            first.val=middle.val;
+            middle.val=t;
+        }
     }
 }
