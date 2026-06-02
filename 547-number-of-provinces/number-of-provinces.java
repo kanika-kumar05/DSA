c60f4983456262e2c3de5ce451abc36bfdfa05b3
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-06-02 12:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (56.73%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -5,18 +5,18 @@ class Solution {
         int count=0;
 
         for(int i=0;i<n;i++){
-            if(vis[i]==false){
+            if(!vis[i]){
                 count++;
                 dfs(i,vis,isConnected);
             }
         }
         return count;
     }
-    public static void dfs(int city,boolean[] vis, int[][] isConnected){
+    public static void dfs(int city, boolean[] vis,int[][] isConnected){
         vis[city]=true;
 
         for(int i=0;i<isConnected.length;i++){
-            if(isConnected[city][i]==1 && vis[i]==false){
+            if(isConnected[city][i]==1 && !vis[i]){
                 dfs(i,vis,isConnected);
             }
         }
