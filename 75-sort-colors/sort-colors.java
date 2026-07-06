e1eec4a980a21fe617c3e9d6067bfb985284c9ba
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-07-06 09:57:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (94.92%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void swap(int[] nums,int low,int high){
+        int temp=nums[low];
+        nums[low]=nums[high];
+        nums[high]=temp;
+    }
+    public void sortColors(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        int mid=0;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
+}
