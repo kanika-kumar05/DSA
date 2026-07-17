1038e1029ca22447ed60db5478bcba402e040d52
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-07-17 23:34:27 +0530

    Time: 2 ms (12.34%) | Memory: 46.6 MB (91.05%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,22 +1,39 @@
+class Pair{
+    int row,col,tm;
+    Pair(int row,int col){
+        this.row=row;
+        this.col=col;
+    }
+}
 class Solution {
-    public void dfs(int sr, int sc,int[][] image, int[][] ans, int color, int init ){
-        int[] dRow={-1,0,1,0};
-        int[] dCol={0,1,0,-1};
-        ans[sr][sc]=color;
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n=image.length;
         int m=image[0].length;
-        for(int i=0;i<4;i++){
-            int r=sr+dRow[i];
-            int c=sc+dCol[i];
-            if(r>=0 && r<n && c>=0 && c<m && ans[r][c]!=color && image[r][c]==init){
-                dfs(r,c,image,ans,color,init);
+        int[][] vis=new int[n][m];
+        Queue<Pair>q=new LinkedList<>();
+        q.add(new Pair(sr,sc));
+
+        int orig=image[sr][sc];
+        vis[sr][sc]=1;
+        image[sr][sc]=color;
+        
+        int[] dRow={-1,0,1,0};
+        int[] dCol={0,1,0,-1};
+        while(!q.isEmpty()){
+            int r=q.peek().row;
+            int c=q.peek().col;
+            q.remove();
+            for(int i=0;i<4;i++){
+                int nRow=r+dRow[i];
+                int nCol=c+dCol[i];
+                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && image[nRow][nCol]==orig && vis[nRow][nCol]==0){
+                    q.add(new Pair(nRow,nCol));
+                    vis[nRow][nCol]=1;
+                    image[nRow][nCol]=color;
+                }
             }
         }
-    }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int init=image[sr][sc];
-        int[][] ans=image;
-        dfs(sr,sc,image,ans,color,init);
-        return ans;
+        return image;
+
     }
 }
