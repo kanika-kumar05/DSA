2c44b513df426858da65ecc73579ae316ab1e0ef
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-07-21 11:10:36 +0530

    Time: 4 ms (46.40%) | Memory: 51.7 MB (81.38%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,47 +1,29 @@
-class Pair{
-    int row,col;
-    Pair(int row,int col){
-        this.row=row;
-        this.col=col;
-    }
-}
 class Solution {
-    public static void bfs(int r,int c,char[][]grid, int[][] vis,int[] dRow, int[] dCol){
-        vis[r][c]=1;
+    public void dfs(int i, int j,int[][] vis,char[][] grid,int[] dCol,int[] dRow){
+        vis[i][j]=1;
         int n=grid.length;
         int m=grid[0].length;
-        Queue<Pair> q=new LinkedList<>();
-        q.add(new Pair(r,c));
-        while(!q.isEmpty()){
-            int ro=q.peek().row;
-            int co=q.peek().col;
-            q.remove();
-            for(int i=0;i<4;i++){
-                    int nRow=ro+dRow[i];
-                    int nCol=co+dCol[i];
-                    if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && grid[nRow][nCol]=='1' &&
-                        vis[nRow][nCol]==0){
-                            vis[nRow][nCol]=1;
-                            q.add(new Pair(nRow,nCol));
-                        }
-                }
+        for(int k=0;k<4;k++){
+            int nRow=i+dRow[k];
+            int nCol=j+dCol[k];
+            if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && vis[nRow][nCol]==0 && grid[nRow][nCol]=='1'){
+                dfs(nRow,nCol,vis,grid,dCol,dRow);
             }
         }
 
-    
-
+    }
     public int numIslands(char[][] grid) {
         int n=grid.length;
         int m=grid[0].length;
         int[][] vis=new int[n][m];
         int count=0;
-        int[] dRow={-1,0,1,0};
-        int[] dCol={0,1,0,-1};
+        int[]dRow={-1,0,1,0};
+        int[]dCol={0,1,0,-1};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(vis[i][j]==0 && grid[i][j]=='1'){
+                    dfs(i,j,vis,grid,dCol,dRow);
                     count++;
-                    bfs(i,j,grid,vis,dRow,dCol);
                 }
             }
         }
