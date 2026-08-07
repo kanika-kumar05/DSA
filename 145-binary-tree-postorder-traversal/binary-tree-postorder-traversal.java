be1ffae84f59ccc9d3e9af01bdad626be27e3dd4
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-08-07 11:29:53 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (15.51%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -64,9 +64,9 @@ class Solution {
         Stack<TreeNode> st2=new Stack<>();
         st1.push(root);
         while(!st1.isEmpty()){
-            TreeNode temp=st1.peek();
+            TreeNode temp=st1.pop();
             st2.push(temp);
-            st1.pop();
+            
             if(temp.left!=null)st1.push(temp.left);
             if(temp.right!=null)st1.push(temp.right);
 
