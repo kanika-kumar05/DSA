07515bace665afee6a661d35042b3ad42ad8b113
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-08-18 16:01:50 +0530

    Time: 55 ms (42.99%) | Memory: 68.6 MB (85.89%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int l=0,maxlen=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int r=0;r<fruits.length;r++){
+            map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
+            if(map.size()>2){
+                map.put(fruits[l],map.get(fruits[l])-1);
+                
+                if(map.get(fruits[l])==0){
+                    map.remove(fruits[l]);
+                }
+                l++;
+            }
+            maxlen=Math.max(maxlen,r-l+1);
+        }
+        return maxlen;
+    }
+}
