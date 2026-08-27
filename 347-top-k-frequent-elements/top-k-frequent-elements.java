b004b38c08851642647ac64b2e8d267d572309f6
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-08-27 18:45:19 +0530

    Time: 16 ms (38.73%) | Memory: 47.5 MB (82.50%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        PriorityQueue<Integer> pq=new PriorityQueue((e1,e2)->map.get(e1)-map.get(e2));
 
-        for(int ele:map.keySet()){
-            pq.add(ele);
-            if(pq.size()>k){
-                pq.poll();
-            }
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Count frequency
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        int[] res=new int[k];
+
+        // Max Heap based on frequency
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>((e1, e2) -> map.get(e2) - map.get(e1));
+
+        // Add all elements
+        for (int ele : map.keySet()) {
+            pq.add(ele);
+        }
+
+        // Get k most frequent elements
+        int[] res = new int[k];
+
         int i=0;
-        while(!pq.isEmpty()){
+        while(i<k){
             res[i]=pq.poll();
             i++;
         }
