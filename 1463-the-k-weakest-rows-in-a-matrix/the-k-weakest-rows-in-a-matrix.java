ff6bdbf27cce4ed0cf75bd408e8eeb4050f7bdd5
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-08-28 12:09:02 +0530

    Time: 2 ms (93.18%) | Memory: 46.5 MB (67.86%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -3,35 +3,39 @@ class Solution {
         int r = mat.length;
         int c = mat[0].length;
 
-        PriorityQueue<int[]> pq=new PriorityQueue<>((e1,e2)->{
-            if(e1[0]==e2[0]){
-                return e2[1]-e1[1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e2[1] - e1[1];   // larger index first
             }
-            return e2[0]-e1[0];
+            return e2[0] - e1[0];       // more soldiers first
         });
 
+        // Add all rows
         for (int i = 0; i < r; i++) {
             int count = 0;
+
             for (int j = 0; j < c; j++) {
                 if (mat[i][j] == 0)
                     break;
+
                 count++;
             }
-            pq.add(new int[]{
-                count,i
-            });
-            if(pq.size()>k){
-                pq.poll();
-            }
+
+            pq.add(new int[]{count, i});
         }
 
-        
-
-        int[] res=new int[k];
-        for(int i=k-1;i>=0;i--){
-            res[i]=pq.poll()[1];
+        // Remove strongest r-k rows
+        for (int i = 0; i < r - k; i++) {
+            pq.poll();
         }
+
+        // Remaining k rows are the weakest
+        int[] res = new int[k];
+
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = pq.poll()[1];
+        }
+
         return res;
     }
-
 }
