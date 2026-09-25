cee715cf056a22bcf7b3c04bc815359d68170abf
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-09-25 15:45:05 +0530

    Time: 1 ms (99.62%) | Memory: 43 MB (33.40%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -1,17 +1,19 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int num:stones){
-            pq.add(num);
+        for(int i=0;i<stones.length;i++){
+            pq.add(stones[i]);
         }
-        if(pq.size()==1) return pq.peek();
         while(pq.size()>1){
-            int temp1=pq.poll();
-            int temp2=pq.poll();
-            if(temp1!=temp2){
-                pq.add(Math.abs(temp1-temp2));
+            int t1=pq.poll();
+            int t2=pq.poll();
+            if(t1==t2){
+                pq.add(t1-t2);
             }
-        } 
-        return pq.isEmpty()?0:pq.peek();
+            else{
+                pq.add(Math.abs(t1-t2));
+            }
+        }
+        return pq.peek();
     }
 }
