80f16b72d4c44dd7dd266780f3f243cad729a783
kanika-kumar05 <kanikakumar05july@gmail.com>
2026-09-25 15:56:32 +0530

    Time: 3 ms (62.70%) | Memory: 46.5 MB (66.46%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -1,41 +1,38 @@
 class Solution {
+    public int count(int[] row){
+        int low=0,high=row.length-1;
+        int index=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(row[mid]==1){
+                index=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
+
+        }
+        return index+1;
+    }
     public int[] kWeakestRows(int[][] mat, int k) {
-        int r = mat.length;
-        int c = mat[0].length;
-
-        PriorityQueue<int[]> pq = new PriorityQueue<>((e1, e2) -> {
-            if (e1[0] == e2[0]) {
-                return e2[1] - e1[1];   // larger index first
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<mat.length;i++){
+            int ones=count(mat[i]);
+            hm.put(i,ones);
+        }
+        PriorityQueue<Integer> pq=new PriorityQueue<>((e1,e2)->{
+            if(hm.get(e1)!=hm.get(e2)){
+                return hm.get(e1)-hm.get(e2);
             }
-            return e2[0] - e1[0];       // more soldiers first
+            return e1-e2;
         });
+        for(int ele:hm.keySet()){
+            pq.add(ele);
 
-        // Add all rows
-        for (int i = 0; i < r; i++) {
-            int count = 0;
-
-            for (int j = 0; j < c; j++) {
-                if (mat[i][j] == 0)
-                    break;
-
-                count++;
-            }
-
-            pq.add(new int[]{count, i});
         }
-
-        // Remove strongest r-k rows
-        for (int i = 0; i < r - k; i++) {
-            pq.poll();
+        int[] res=new int[k];
+        for(int i=0;i<k;i++){
+            res[i]=pq.poll();
         }
-
-        // Remaining k rows are the weakest
-        int[] res = new int[k];
-
-        for (int i = k - 1; i >= 0; i--) {
-            res[i] = pq.poll()[1];
-        }
-
         return res;
     }
 }
